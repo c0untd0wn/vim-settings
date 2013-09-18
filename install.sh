71aad89c9b9832fa2779f6af7ef6b9769a4dd6b0
Beomki Lee <c0untd0wn@wafflestudio.com>
2013-09-19 00:28:44 +0900

    modified install script

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+echo "Remove Previous Settings"
+rm -rf ~/.vim
+rm -rf ~/.vimrc
+
 echo "Copying .vimrc"
 cp vimrc ~/.vimrc
 
 echo "Copying other folders"
 mkdir -p ~/.vim
-rsync -rv --exclude=.git * ~/.vim 
+rsync -rv --exclude=.git * ~/.vim
